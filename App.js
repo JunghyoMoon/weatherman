0ad10196890e63cc70487710357ff7222191c895
JungHyoMoon <dsinhome70@gmail.com>
2021-09-24 14:22:19 +0900

    api키 등록 대기중..

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import * as Location from "expo-location";
+import axios from "axios";
 // what is react native? => 자바스크립트를 이용해서 네이티브 엔진에게 메세지를 보냄?
 // 자바스크립트와 모바일 기기의 다리 역할을 해줌.
 // 편리하지만, 중간에 '통역사'가 반드시 필요한 만큼 성능에는 문제가 있을 수 있음.
@@ -9,14 +10,21 @@ import { Alert } from "react-native";
 // components
 import Loader from "./Loader";
 
-const API_KEY = "d24a46f71a64d072d08f9951f8ad41cd";
+const API_KEY = "d24a46f71a64d072d08f9951f8ad41cd"; // change it to new one.
 
 // RN만의 규칙이 당연히 존재. => 예를 들면, 컴포넌트의 container, <div>같은 것들은 '<View>'로 처리하게 되어 있음.
 // 텍스트는 전부 <Text>안에 있어야 하고.. 이외에도 리액트와 다른 차이점들이 존재함.
 class App extends React.Component {
     state = {
         isLoading: true,
-        location: { lat: null, long: null },
+        data: {},
+    };
+
+    getWeather = async (lat, long) => {
+        const res = await axios.get(
+            `http://openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`
+        );
+        console.log(res);
     };
 
     getLocation = async () => {
@@ -25,8 +33,7 @@ class App extends React.Component {
             const {
                 coords: { latitude: lat, longitude: long },
             } = await Location.getCurrentPositionAsync();
-            this.setState({ location: { lat, long } });
-            // todo: send coords to API, and get weather info.
+            this.getWeather(lat, long);
         } catch (error) {
             Alert.alert("Cannot get location!", "so sad ㅠㅠ");
         } finally {
@@ -39,8 +46,7 @@ class App extends React.Component {
     }
 
     render() {
-        const { isLoading, location } = this.state;
-        console.log(location);
+        const { isLoading, data } = this.state;
         return isLoading ? <Loader /> : null;
     }
 }
